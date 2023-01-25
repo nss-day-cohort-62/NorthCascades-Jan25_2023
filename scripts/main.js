4ad08ff042cc8abe53333e46297910b51657d0f4
Jessalynn Whyte <jessalynn.whyte@gmail.com>
2023-01-25 11:38:08 -0600

    Algorithmic Thinking

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-
+/* Requirements:
+* Have an array of users who watch TV. Each user will have
+    a name, an age, and favorite genre.
+* Have an array of TV shows. Each show will have a title,
+    star actor name, genre, and original air date.
+* Have an array of all possible genres.
+* When displaying users, show their name as a header, and
+    all recommended shows underneath.
+* When displaying shows, show the title, genre name, and
+    star actor for the show.
+*/
