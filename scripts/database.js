9dffca1710b5f72c45409b2fb6c82634dfe0008c
Dylan Glynn <dylan.hawes.glynn+github@gmail.com>
2023-01-26 11:58:41 -0600

    updates

--- scripts/database.js
+++ scripts/database.js
@@ -1,68 +1,86 @@
 const database = {
-  shows: [{
+  shows:[{
     id: 1,
     title: "White Lotus",
-    actorId: "",
-    genreId: ,
-    airDate: ""
+    actorId: 1,
+    genreId: 1,
+    airDate: "1/2/2023"
   },
   {
     id: 2,
     title: "The Wire",
-    actorId: ,
-    genreId: ,
-    airDate: ""
+    actorId: 2,
+    genreId: 2,
+    airDate: "2/3/2010"
   },
   {
     id: 3,
     title: "The Bear",
-    actorId: ,
-    genreId: ,
-    airDate: ""
+    actorId: 3,
+    genreId: 3,
+    airDate: "3/4/2021"
   }
-  ],
+],
   actors: [{
-    id: 4,
-    name:
-    },
-  {
-    id: 5,
-    name:
-    },
-  {
-    id: 6,
-    name:
-    }
-  ],
-  users: [{
-    id: 7,
-    name: ,
-    genreId: ,
-    age:
-    },
-  {
-    id: 8,
-    name: ,
-    genreId: ,
-    age:
-    },
-  {
-    id: 9,
-    name: ,
-    genreId: ,
-    age:
-    }
-  ],
-  genres: [{
     id: 1,
-    name: ""
-  }, {
+    name: "Jennifer Coolidge"
+  },
+  {
     id: 2,
-    name: ""
-  }]
+    name: "Idris Elba"
+  },
+  {
+    id: 3,
+    name: "Gary Garrison"
+  }
+],
+  users: [{
+    id: 1,
+    name: "Billy Bailey",
+    genreId: 1,
+    age: 30
+  },
+  {
+    id: 2,
+    name: "Norm Normy",
+    genreId: 2,
+    age: 40
+  },
+  {
+    id: 3,
+    name: "Jill Jilly",
+    genreId: 3,
+    age: 50
+  }
+],
+genres: [{
+  id: 1,
+  name: "Comedy"  
+},
+{
+  id: 2,
+  name: "Crime"  
+},
+{
+  id: 3,
+  name: "Drama"
+}
+]
 
 }
 
 export const getShows = () => {
-  return null
+  return database.shows.map(show => ({...show}))
+}
+
+export const getActors = () => {
+  return database.actors.map(actor => ({...actor}))
+}
+
+export const getUsers = () => {
+  return database.users.map(user => ({...user}))
+}
+
+export const getGenres = () => {
+  return database.genres.map(genre => ({...genre}))
 }
